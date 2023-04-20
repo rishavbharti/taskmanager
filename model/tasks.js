cb5acbf856b095a72ccaebd81c24585b1ce35626
Rishav Bharti <rishavbharti21@gmail.com>
2023-04-20 20:02:03 +0530

    feat: create schema of tasks

---
+++ model/tasks.js
@@ -0,0 +1,29 @@
+const Database = require('.././db');
+
+const taskSchema = {
+  title: {
+    type: 'String',
+    trim: true,
+    minLength: 3,
+    maxLength: 100,
+    required: true,
+  },
+  description: {
+    type: 'String',
+    trim: true,
+    minLength: 3,
+    maxLength: 1000,
+    required: true,
+  },
+  status: {
+    type: 'Boolean',
+    default: false,
+  },
+  priority: {
+    type: 'String',
+    enum: ['Low', 'Medium', 'High'],
+    default: 'Medium',
+  },
+};
+
+module.exports = new Database().model('Tasks', taskSchema);

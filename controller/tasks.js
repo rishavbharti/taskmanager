9c52ba6157c92b2a4ca3b7390c580e36e20ee62c
Rishav Bharti <rishavbharti21@gmail.com>
2023-04-20 23:00:35 +0530

    feat: add routes for creating & getting tasks

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,13 +1,30 @@
+const tasks = require('../model/tasks');
+
 // @desc    Get all tasks
 // @route   GET /api/tasks
 // @access  Public
 const getAllTasks = async (req, res) => {
   try {
-    return res.status(201).send('All tasks');
+    const allTasks = tasks.find();
+    res.status(201).send(allTasks);
   } catch (error) {
-    console.error(error);
-    return res.status(400).json(error);
+    console.error('error ', error.message);
+    res.status(400).json(error);
   }
 };
 
-module.exports = { getAllTasks };
+// @desc    Create a new task
+// @route   POST /api/tasks
+// @access  Public
+const createTask = async (req, res) => {
+  try {
+    const body = req.body;
+    const result = tasks.write(body);
+    res.status(200).send(result);
+  } catch (error) {
+    console.error('error ', error.message);
+    res.status(400).json(error.message);
+  }
+};
+
+module.exports = { getAllTasks, createTask };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { getAllTasks } = require('../controller/tasks');
+const { getAllTasks, createTask } = require('../controller/tasks');
 const router = express.Router();
 
 router.get('/tasks', getAllTasks);
+
+router.post('/tasks', createTask);
 
 module.exports = router;

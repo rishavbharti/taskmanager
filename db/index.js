27d455a248023811187b9d80d3024b83588cf2c9
Rishav Bharti <rishavbharti21@gmail.com>
2023-04-20 20:01:27 +0530

    feat: create a db class for storing data in-memory. add `validate` & `write` function

---
+++ db/index.js
@@ -0,0 +1,97 @@
+class Database {
+  constructor() {}
+
+  model(name, schema) {
+    this[name] = {
+      _schema: schema,
+      store: [],
+    };
+
+    return this;
+  }
+
+  validate(name, data) {
+    for (const [field, properties] of Object.entries(this[name]['_schema'])) {
+      for (const [property, value] of Object.entries(properties)) {
+        switch (property) {
+          case 'required': {
+            if (!data.hasOwnProperty(field)) {
+              throw new Error(`Missing required field ${field}.`);
+            }
+            break;
+          }
+
+          case 'type': {
+            if (
+              data.hasOwnProperty(field) &&
+              typeof data[field] !== value.toLowerCase()
+            ) {
+              throw new Error(`${field} should be of type ${value}.`);
+            }
+            break;
+          }
+
+          case 'minLength': {
+            if (data.hasOwnProperty(field) && data[field].length < value) {
+              throw new Error(
+                `${field} should have a minimum length of ${value}.`
+              );
+            }
+            break;
+          }
+
+          case 'maxLength': {
+            if (data.hasOwnProperty(field) && data[field].length > value) {
+              throw new Error(
+                `${field} can have a maximum length of ${value}.`
+              );
+            }
+            break;
+          }
+
+          case 'trim': {
+            if (typeof data[field] === 'string') {
+              data[field] = data[field].trim();
+            }
+            break;
+          }
+
+          case 'default': {
+            data[field] = value;
+            break;
+          }
+
+          case 'enum': {
+            if (data.hasOwnProperty(field) && !value.includes(data[field])) {
+              throw new Error(
+                `${field} is an Enum. Should be one of ${value}.`
+              );
+            }
+            break;
+          }
+
+          default:
+            break;
+        }
+      }
+    }
+
+    data.id = Date.now();
+    data.createdAt = new Date();
+    data.updatedAt = new Date();
+  }
+
+  write(data) {
+    try {
+      const schemaName = Object.keys(this)[0];
+
+      this.validate(schemaName, data);
+      this[schemaName].store.push(data);
+      return data;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+}
+
+module.exports = Database;
